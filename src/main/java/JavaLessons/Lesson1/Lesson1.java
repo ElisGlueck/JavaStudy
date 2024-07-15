18c01cf82d8fe116e780a07c8e1c34005aec09b6
ElisGlueck <elisgluck@gmail.comcolor.ui>
2024-07-15 18:19:26 +0300

    First task v3 Second task

--- src/main/java/JavaLessons/Lesson1/Lesson1.java
+++ src/main/java/JavaLessons/Lesson1/Lesson1.java
@@ -4,9 +4,12 @@ public class Lesson1 {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        System.out.println(checkSumSignV2(-15, 6));
         printColor();
+        printColorV2(120);
         compareNumbers();
     }
+
 
     public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple\n");
@@ -18,6 +21,10 @@ public class Lesson1 {
             System.out.println("Сумма положительная");
         } else System.out.println("Сумма отрицательная");
     }
+    private static boolean checkSumSignV2(int first, int second) {
+        return first + second >= 0;
+    }
+
     public static void printColor() {
         int value = 333;
         if (value <= 0) {
@@ -27,6 +34,11 @@ public class Lesson1 {
         } else {
             System.out.println("Зеленый");
         }
+    }
+
+    public static void printColorV2(int value) {
+        int q = value == 10 ? 100500 : 0;
+        System.out.println(value <= 0 ? "Red" : value <= 100 ? "Yellow" : "Green");
     }
     public static void compareNumbers() {
         int a = 46;
